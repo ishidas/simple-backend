449913bb74e9f883000c317fc4bc7ea52d202a93
Sawako Sagliano <sawako.ishida1@gmail.com>
2021-01-23 18:32:46 +0900

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simple-backend
+
+## Setup
+1. place your data at the root of the project (see example.csv)
+2.
+```
+npm i
+```
+
+## Run
+```
+npm start
+```
--- index.js
+++ index.js
@@ -9,24 +9,16 @@ app.use(cors());
 app.use(bodyParser.json())
 
 app.get('/data', async (req, res) => {
-    
     try {
         const rangeTotal = await getCSV()
         .then(data => {
             const allData = data;
             let result = 0;
             let { start, end } = req.query;
-            console.log('start', start);
-            console.log('end', end);
             const startIndex = _.findIndex(allData, (d) => {
                  return d['Order Date'] === start;
                 });
             const endIndex = _.findIndex(allData, (d) => d['Order Date'] === end);
-            // console.log('startIndex???', startIndex);
-            // console.log('startIndex???', allData[startIndex]);
-            // console.log('endIndex??', endIndex);
-            // console.log('endIndex??', allData[endIndex]);
-
             let total = 0;
             for (let i = startIndex; i < endIndex; i++) {
                 total = parseFloat(allData[i].Total);
@@ -34,14 +26,12 @@ app.get('/data', async (req, res) => {
             }
             return result.toFixed(2);
         })
-        .catch((err) => {
-            console.log('errr', err);
-        });
+        .catch(console.error);
         res.send({
             rangeTotal,
         });
     } catch(e) {
-        console.log('err get data', e);
+        console.error('err in get /data', e);
     }
 
 })
